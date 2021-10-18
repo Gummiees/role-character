7fbf9689609cc9d1251abf999754642000d91066
Xavier Caro Miro <xavicaromiro@gmail.com>
2021-10-18 23:48:28 +0200

    Initial models for the interaction in between stats and skills.

--- src/app/shared/models/skill.model.ts
+++ src/app/shared/models/skill.model.ts
@@ -1,5 +1,22 @@
+import { TurnPhases } from './turn.model';
+
+export interface StatAffected {
+  id?: number;
+  statId: number;
+  value: number;
+}
+
 export interface Skill {
-  skillName: string;
-  percentage: number;
-  result: number;
+  userId?: string;
+  id?: number;
+  name?: string;
+  description?: string;
+  active: boolean;
+  doesRollDice: boolean;
+  whenRollDice: TurnPhases;
+  permanent: boolean;
+  turnsLeft: number;
+  stats: StatAffected[];
+  level?: number;
+  caster_name?: string;
 }
---
+++ src/app/shared/models/turn.model.ts
@@ -0,0 +1,6 @@
+export enum TurnPhases {
+  START = 'START',
+  COMBAT = 'COMBAT',
+  AFTER_COMBAT = 'AFTER_COMBAT',
+  END = 'END'
+}
